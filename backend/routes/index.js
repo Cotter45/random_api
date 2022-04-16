ecfbb3bd24374ba06f59ae2587788b51e5ec13ab
Sean <sd.cotter45@gmail.com>
2022-04-16 00:06:27 -0400

    still had a csrf token function in there

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const asyncHandler = require("express-async-handler");
 const router = express.Router();
 const apiRouter = require("./api");
 
@@ -11,7 +10,6 @@ if (process.env.NODE_ENV === "production") {
   const path = require("path");
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
     res.sendFile(
       path.resolve(__dirname, "../../frontend", "build", "index.html")
     );
@@ -22,7 +20,6 @@ if (process.env.NODE_ENV === "production") {
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
     res.sendFile(
       path.resolve(__dirname, "../../frontend", "build", "index.html")
     );
