75907b1aaa8f935c937e7a9e4f5584bdc52cc742
Sean <sd.cotter45@gmail.com>
2022-04-17 08:04:07 -0400

    removed the auth stuff, going to make the logs public as well

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,10 @@
 const router = require("express").Router();
-const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
 const parksRouter = require("./parks.js");
 const cocktailsRouter = require("./cocktails.js");
 
 router.use("/cocktails", cocktailsRouter);
 router.use("/parks", parksRouter);
-router.use("/session", sessionRouter);
 router.use("/users", usersRouter);
 
 module.exports = router;
